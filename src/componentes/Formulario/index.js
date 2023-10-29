a503334b0099e3badeff516cb17086ecd5a38418
vmcustodio <van.custodio.vc@gmail.com>
2023-10-29 01:11:35 -0300

    feat: refactoring

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Botao from "../Botao";
-import CampoTexto from "../CampoTexto";
+import Campo from "../Campo";
 import ListaSuspensa from "../ListaSuspensa";
 import "./formulario.css";
 
@@ -27,21 +27,21 @@ const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
     <section className="formulario-container">
       <form className="formulario" onSubmit={aoSubmeter}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Nome"
           placeholder="Digite seu nome "
           valor={nome}
           aoAlterado={(valor) => setNome(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Cargo"
           placeholder="Digite seu cargo "
           valor={cargo}
           aoAlterado={(valor) => setCargo(valor)}
         />
-        <CampoTexto
+        <Campo
           label="Imagem"
           placeholder="Informe o endereço da imagem "
           aoAlterado={(valor) => setImagem(valor)}
@@ -63,16 +63,17 @@ const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
         }}
       >
         <h2>Preencha os dados para criar um novo time.</h2>
-        <CampoTexto
+        <Campo
           obrigatorio
           label="Nome"
           placeholder="Digite o nome do time"
           valor={nomeTime}
           aoAlterado={(valor) => setNomeTime(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio
           label="Cor"
+          type="color"
           placeholder="Digite a cor do time"
           valor={corTime}
           aoAlterado={(valor) => setCorTime(valor)}
