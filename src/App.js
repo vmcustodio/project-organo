a3d53dd9131aa3c313513205e74fc4835a37fe26
vmcustodio <van.custodio.vc@gmail.com>
2023-10-29 00:48:19 -0300

    feat: remove colaborador

--- src/App.js
+++ src/App.js
@@ -240,8 +240,10 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial);
 
-  function deletarColaborador() {
-    console.log("ok");
+  function deletarColaborador(id) {
+    setColaboradores(
+      colaboradores.filter((colaborador) => colaborador.id !== id)
+    );
   }
 
   function mudarCorDoTime(cor, id) {
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -4,7 +4,10 @@ import { BsFillTrashFill } from "react-icons/bs";
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
   return (
     <div className="colaborador">
-      <BsFillTrashFill className="deletar" onClick={aoDeletar} />
+      <BsFillTrashFill
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+      />
       <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={colaborador.imagem} alt={colaborador.nome} />
       </div>
